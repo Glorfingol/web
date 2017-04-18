a9f352727746867b123da4d4a38d00535a6c58cf
lperrod <perrod.louis@gmail.com>
2017-04-18 13:27:05 +0200

    Refacto error response

--- src/main/java/cmpl/web/model/news/rest/news/NewsEntryResponse.java
+++ src/main/java/cmpl/web/model/news/rest/news/NewsEntryResponse.java
@@ -1,13 +1,11 @@
 package cmpl.web.model.news.rest.news;
 
 import cmpl.web.model.news.dto.NewsEntryDTO;
-import cmpl.web.model.news.error.Error;
 import cmpl.web.model.news.rest.BaseResponse;
 
 public class NewsEntryResponse extends BaseResponse {
 
   private NewsEntryDTO newsEntry;
-  private Error error;
 
   public NewsEntryDTO getNewsEntry() {
     return newsEntry;
@@ -15,14 +13,6 @@ public class NewsEntryResponse extends BaseResponse {
 
   public void setNewsEntry(NewsEntryDTO newsEntry) {
     this.newsEntry = newsEntry;
-  }
-
-  public Error getError() {
-    return error;
-  }
-
-  public void setError(Error error) {
-    this.error = error;
   }
 
 }
