3723c4153d74724fa94155853fcb864259b9fb14
lperrod <perrod.louis@gmail.com>
2017-06-13 13:57:23 +0200

    Facebook

--- src/main/resources/static/js/importFacebook.js
+++ src/main/resources/static/js/importFacebook.js
@@ -1,24 +1,52 @@
 var postsToImport = [];
 $(document).ready(function(){
-	$("input[type='checkbox']").each(function(){
-		if(this.id !== 'importAll'){
+	
+	var checkBoxSelector = "input[type='checkbox']";
+	$(checkBoxSelector).each(function(){
+		if(!isImportAllCheckBox(this.id)){
 			$(this).change(function(){
 				var fullId = this.id;
 				var facebookId = fullId.split('-')[1];
 				toggleImport(facebookId);
-			})
+			});
 		}
-	})
+		if(isImportAllCheckBox(this.id)){
+			$(this).change(function(){
+				toggleAllImport();
+			});
+		}
+	});
 })
 
 
-
+function toggleAllImport(){
+	$(checkBoxSelector).each(function(){
+		if(!isImportAllCheckBox(this.id)){
+			var fullId = this.id;
+			var facebookId = fullId.split('-')[1];
+			if(isAllImportChecked()){
+				if(!computeIsToImport(facebookId)){
+					$(this).prop("checked", true);
+					toggleImport(facebookId);
+				}
+			}else{
+				if(computeIsToImport(facebookId)){
+					$(this).prop("checked", false);
+					toggleImport(facebookId);
+				}
+			}
+		}
+	});
+}
 
 function toggleImport(facebookId){
 	
 	var isToImport = computeIsToImport(facebookId);
 	if(isToImport){
-		postsToImport.push(computePostToImport(facebookId));
+		var post = computePostToImport(facebookId);
+		if(!isAlreadyInImport(facebookId)){
+			postsToImport.push(post);
+		}
 	}else{
 		postsToImport.forEach(function(post){
 			if(post.facebookId === facebookId){
@@ -27,9 +55,28 @@ function toggleImport(facebookId){
 					postsToImport.splice(index, 1);
 				}
 			}
-		})
+		});
 	}
-	
+}
+
+function isAlreadyInImport(facebookId){
+	var isAlreadyInImport = false;
+	postsToImport.forEach(function(post){
+		if(post.facebookId === facebookId){
+			isAlreadyInImport = true;
+		}
+	});
+	return isAlreadyInImport;
+}
+
+function isImportAllCheckBox(checkboxId){
+	return checkboxId === 'importAll';
+}
+
+function isAllImportChecked(){
+	var checkboxId = '#importAll';
+	var checkboxInput = $(checkboxId);
+	return checkboxInput.is(":checked");
 }
 
 function computeIsToImport(facebookId){
