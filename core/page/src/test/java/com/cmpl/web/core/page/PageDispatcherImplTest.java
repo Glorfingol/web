ff2100fbb4bbc9ff8430b176fb89821734e75f4c
lperrod <lperrod@cardiweb.com>
2018-02-08 15:19:17 +0100

    UTs correction

--- core/page/src/test/java/com/cmpl/web/core/page/PageDispatcherImplTest.java
+++ core/page/src/test/java/com/cmpl/web/core/page/PageDispatcherImplTest.java
@@ -31,7 +31,7 @@ public class PageDispatcherImplTest {
   @Test
   public void testCreateEntity_No_Error() throws Exception {
 
-    PageCreateForm form = PageCreateFormBuilder.create().build();
+    PageCreateForm form = PageCreateFormBuilder.create().localeCode(Locale.FRANCE.getLanguage()).build();
 
     BDDMockito.given(validator.validateCreate(BDDMockito.any(PageCreateForm.class), BDDMockito.any(Locale.class)))
         .willReturn(null);
@@ -39,13 +39,14 @@ public class PageDispatcherImplTest {
     PageDTO page = PageDTOBuilder.create().build();
     PageResponse response = PageResponseBuilder.create().page(page).build();
     BDDMockito.given(translator.fromCreateFormToDTO(BDDMockito.any(PageCreateForm.class))).willReturn(page);
-    BDDMockito.given(pageService.createEntity(BDDMockito.any(PageDTO.class))).willReturn(page);
+    BDDMockito.given(pageService.createEntity(BDDMockito.any(PageDTO.class), BDDMockito.anyString())).willReturn(page);
     BDDMockito.given(translator.fromDTOToResponse(BDDMockito.any(PageDTO.class))).willReturn(response);
 
     Assert.assertEquals(response.getPage(), dispatcher.createEntity(form, Locale.FRANCE).getPage());
     BDDMockito.verify(validator, BDDMockito.times(1)).validateCreate(BDDMockito.any(PageCreateForm.class),
         BDDMockito.any(Locale.class));
-    BDDMockito.verify(pageService, BDDMockito.times(1)).createEntity(BDDMockito.any(PageDTO.class));
+    BDDMockito.verify(pageService, BDDMockito.times(1)).createEntity(BDDMockito.any(PageDTO.class),
+        BDDMockito.anyString());
     BDDMockito.verify(translator, BDDMockito.times(1)).fromCreateFormToDTO(BDDMockito.any(PageCreateForm.class));
     BDDMockito.verify(translator, BDDMockito.times(1)).fromDTOToResponse(BDDMockito.any(PageDTO.class));
   }
@@ -71,22 +72,23 @@ public class PageDispatcherImplTest {
   @Test
   public void testUpdateEntity_No_Error() throws Exception {
 
-    PageUpdateForm form = PageUpdateFormBuilder.create().id(123456789l).build();
+    PageUpdateForm form = PageUpdateFormBuilder.create().id(123456789l).localeCode(Locale.FRANCE.getLanguage()).build();
 
     BDDMockito.given(validator.validateUpdate(BDDMockito.any(PageUpdateForm.class), BDDMockito.any(Locale.class)))
         .willReturn(null);
 
     PageDTO page = PageDTOBuilder.create().build();
     PageResponse response = PageResponseBuilder.create().page(page).build();
-    BDDMockito.given(pageService.getEntity(BDDMockito.anyLong())).willReturn(page);
-    BDDMockito.given(pageService.updateEntity(BDDMockito.any(PageDTO.class))).willReturn(page);
+    BDDMockito.given(pageService.getEntity(BDDMockito.anyLong(), BDDMockito.anyString())).willReturn(page);
+    BDDMockito.given(pageService.updateEntity(BDDMockito.any(PageDTO.class), BDDMockito.anyString())).willReturn(page);
     BDDMockito.given(translator.fromDTOToResponse(BDDMockito.any(PageDTO.class))).willReturn(response);
 
     Assert.assertEquals(response.getPage(), dispatcher.updateEntity(form, Locale.FRANCE).getPage());
     BDDMockito.verify(validator, BDDMockito.times(1)).validateUpdate(BDDMockito.any(PageUpdateForm.class),
         BDDMockito.any(Locale.class));
-    BDDMockito.verify(pageService, BDDMockito.times(1)).getEntity(BDDMockito.anyLong());
-    BDDMockito.verify(pageService, BDDMockito.times(1)).updateEntity(BDDMockito.any(PageDTO.class));
+    BDDMockito.verify(pageService, BDDMockito.times(1)).getEntity(BDDMockito.anyLong(), BDDMockito.anyString());
+    BDDMockito.verify(pageService, BDDMockito.times(1)).updateEntity(BDDMockito.any(PageDTO.class),
+        BDDMockito.anyString());
     BDDMockito.verify(translator, BDDMockito.times(1)).fromDTOToResponse(BDDMockito.any(PageDTO.class));
   }
 
