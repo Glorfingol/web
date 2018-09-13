083540cf690d5be6a7aa458ce827c72e4c8a392b
lperrod <lperrod@cardiweb.com>
2018-09-13 18:31:32 +0200

    Auditor Aware

--- autoconfigure/src/main/java/com/cmpl/web/configuration/EnableCMPLWeb.java
+++ autoconfigure/src/main/java/com/cmpl/web/configuration/EnableCMPLWeb.java
@@ -12,6 +12,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -22,6 +23,7 @@ import org.springframework.context.annotation.Import;
 @EnableAutoConfiguration
 @ComponentScan
 @EnableCaching
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @Import(EnableCMPLWebImportSelector.class)
 public @interface EnableCMPLWeb {
 
