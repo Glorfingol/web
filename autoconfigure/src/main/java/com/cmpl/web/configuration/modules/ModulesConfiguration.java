57ecbf22f92b5e16b20dc6276457830146877416
lperrod <perrod.louis@gmail.com>
2018-10-17 20:31:04 +0200

    Connexion Facebook

--- autoconfigure/src/main/java/com/cmpl/web/configuration/modules/ModulesConfiguration.java
+++ autoconfigure/src/main/java/com/cmpl/web/configuration/modules/ModulesConfiguration.java
@@ -6,12 +6,31 @@ import com.cmpl.web.configuration.modules.backup.BackupImportConfiguration;
 import com.cmpl.web.configuration.modules.facebook.FacebookAutoConfiguration;
 import com.cmpl.web.configuration.modules.facebook.FacebookConfiguration;
 import com.cmpl.web.configuration.modules.google.GoogleConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
 
 @Configuration
 @Import({GoogleConfiguration.class, FacebookAutoConfiguration.class, FacebookConfiguration.class,
-    BackupConfiguration.class, BackupExportConfiguration.class, BackupImportConfiguration.class})
+        BackupConfiguration.class, BackupExportConfiguration.class, BackupImportConfiguration.class})
 public class ModulesConfiguration {
+
+
+    @Value(value = "${baseUrl}")
+    private String baseUrl;
+
+    @Bean
+    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator,
+                                               ConnectionRepository connectionRepository) {
+        ConnectController controller = new ConnectController(connectionFactoryLocator,
+                connectionRepository);
+        controller.setApplicationUrl(baseUrl);
+        return controller;
+
+    }
 
 }
